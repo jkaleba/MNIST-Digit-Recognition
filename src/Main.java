54dc4e09b0ab4b0cff5cfe09d805367d16df8ca2
WegielWBucie <pondroszejset@gmail.com>
2023-02-10 20:47:00 +0100

    Default parameters.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ class Main {
 
         Data trainingData = new MNISTDataReader().readTrainingData();
 
-        NeuralNetwork network = new NeuralNetwork(new int[]{784, 84, 28, 10});
+        NeuralNetwork network = new NeuralNetwork(new int[]{784, 30, 10});
         Data testData = new MNISTDataReader().readTestData();
 
-        network.SGD(trainingData, 1000, 10, 0.7, testData);
+        network.SGD(trainingData, 30, 10, 1.0, testData);
     }
 }
